43e65a53c4bfc825b108ee96563deee173ae307b
Ven <chrisx84@live.ca>
2025-06-29 08:12:29 -0400

    Initial commit

---
+++ BrainofTrump.cs
@@ -0,0 +1,36 @@
+﻿namespace BrainofTrump
+{
+    public class BrainofTrump
+    {
+        public static void Main(string[] args)
+        {
+            string Country = args.Length > 0 ? args[0] : "Canada";
+            if (Country == "Canada")
+            {
+                Console.WriteLine("I hate Canada because they hate me so I'm going to tariff them and it will be spectacular");
+                HateCanada();
+            }
+            else if (Country == "Mexico")
+            {
+                Console.WriteLine("Mexico I don't know much about so affective immediately I will deport anyone from Mexico");
+            }
+            else if (Country == "USA")
+            {
+                Console.WriteLine("The USA is the best country in the world, believe me!");
+            }
+            else
+            {
+                Console.WriteLine($"I don't know much about {Country}, but I'm sure it's great and the people love me!");
+            }
+        }
+        private static void HateCanada()
+        {
+            int tariff = 25;
+            for (int i = 0; i < 20; i++)
+            {
+                tariff = tariff + (i * 2);
+                Console.WriteLine($"Tariffing Canada at {tariff.ToString()}%");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Brain of Trump
+## JUST A JOKE!
+Did this as a joke! Because I'm Canadian I did some extra work for Canada.
+
+Wanted to show some friends a quick mockup of how csharp works.
+
+Releasing the code to maybe teach others how to code in C# in a funny way.
